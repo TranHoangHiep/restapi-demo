11359e49aa9208f70abb05174f4f88a893d47b5f
hoang hiep <hiepth@mpos.vn>
2021-05-14 16:04:50 +0700

    add and delete api

--- src/main/java/com/studying/restapidemo/controller/UserController.java
+++ src/main/java/com/studying/restapidemo/controller/UserController.java
@@ -5,10 +5,7 @@ import com.studying.restapidemo.model.response.Response;
 import com.studying.restapidemo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("user")
@@ -32,6 +29,35 @@ public class UserController {
             response.setCode(1);
             response.setMessage("Khong thanh cong");
         }
+
+        return response;
+    }
+
+    @PostMapping("add")
+    public Response<User> add(@RequestBody User user){
+        System.out.println("username: " + user.getUsername());
+
+        // add user to database
+
+        //if add user success return response
+        Response<User> response = new Response<>();
+        response.setCode(0);
+        response.setMessage("adding user success");
+        response.setData(user);
+
+        return response;
+    }
+
+    @GetMapping("delete/{id}")
+    public Response delete(@PathVariable("id") int id){
+        System.out.println("id: " + id);
+
+        //delete user by id in service
+
+        // if delete success return response
+        Response response = new Response();
+        response.setCode(0);
+        response.setMessage("success");
 
         return response;
     }
